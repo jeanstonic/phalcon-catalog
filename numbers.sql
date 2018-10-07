0ff6dd4fe24eff7e49eea0bb04574be399022101
ak <ak@solarjetpro.com>
2018-10-07 23:59:47 +0300

    links in footer

--- numbers.sql
+++ numbers.sql
@@ -19,7 +19,7 @@ INSERT INTO products (name) VALUES ('one'), ('two'), ('three'), ('four'), ('five
 ('six'), ('seven'), ('eight'), ('nine'), ('ten'), ('eleven'), ('twelve'), ('thirteen'),
 ('fourteen'), ('fifteen'), ('sixteen'), ('seventeen'), ('eighteen'), ('nineteen'), ('twenty');
 
-INSERT INTO categories (name, parent_category_id) VALUES ('number', NULL), ('even', 1), ('odd', 1), ('prime', 1), ('composite', 1);
+INSERT INTO categories (name, parent_category_id) VALUES ('integer', NULL), ('even', 1), ('odd', 1), ('prime', 1), ('composite', 1);
 
 INSERT INTO category_products (category_id, product_id) VALUES (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6),
 (1, 7), (1, 8), (1, 9), (1, 10), (1, 11), (1, 12), (1, 13), (1, 14), (1, 15), (1, 16), (1, 17), (1, 18), (1, 19),
